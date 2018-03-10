4da823c58767e37be135b39fdfd5bf5d175ad9aa
hirosuke7 <captif7_de_2_anjelot8@ezweb.ne.jp>
2018-03-10 11:17:28 +0900

    fix

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ApplicationController
+
+  def show
+    #Itemモデルのfindメソッドを使用して、パラメータに値に一致するidのデータを検索している
+    #取得したデータを@itemという変数にセットする
+    @item = Item.find(params[:id])
+  end
+
+
+  def index
+    @items = Item.all
+  end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+end
