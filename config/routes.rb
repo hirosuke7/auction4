6ddea13d58898c645204db8500231df398172b5a
hirosuke7 <captif7_de_2_anjelot8@ezweb.ne.jp>
2018-03-10 15:23:59 +0900

    comp1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/items' => 'items#index'
-  post '/items' => 'items#create'   #追加
-  get '/items/new' => 'items#new'   #追加
-  get '/items/:id' => 'items#show'
+ resources :items
+
 end
